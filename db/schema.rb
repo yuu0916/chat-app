6ca3326e6a4c9ed5f2bfd3895b9bb471b3c6a5ae
yuu0916 <131441714+yuu0916@users.noreply.github.com>
2023-04-28 16:55:11 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_28_031757) do
+ActiveRecord::Schema.define(version: 2023_04_28_033844) do
+
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
