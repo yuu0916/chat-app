5e59ebf43ab1d1d67a8fe864b880b2443f6002ab
yuu0916 <131441714+yuu0916@users.noreply.github.com>
2023-04-28 17:52:40 +0900

    チャット画面の編集

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,12 @@
 ActiveRecord::Schema.define(version: 2023_04_28_033844) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -36,4 +40,6 @@ ActiveRecord::Schema.define(version: 2023_04_28_033844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
